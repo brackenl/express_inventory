82ecd14cb43becb88f29e86ee0b0e01a8c595de9
William Bracken <liam.bracken@gmail.com>
2020-11-12 00:12:06 +0000

    Add forms to create new items and update controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 var async = require("async");
+const { body, validationResult } = require("express-validator");
 
 var Category = require("../models/category");
 var Tyre = require("../models/tyre");
@@ -51,13 +52,64 @@ exports.category_detail = function (req, res) {
 
 // Display Category create form on GET.
 exports.category_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category create GET");
+  res.render("category_form", { title: "Create Category" });
 };
 
 // Handle Category create on POST.
-exports.category_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category create POST");
-};
+exports.category_create_post = [
+  // Validate and santise the name and description fields.
+  body("name", "Category name required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a category object with escaped and trimmed data.
+    var category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      Category.findOne({ name: req.body.name }).exec(function (
+        err,
+        found_category
+      ) {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_category) {
+          // Brand exists, redirect to its detail page.
+          res.redirect(found_category.url);
+        } else {
+          category.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // Brand saved. Redirect to brand detail page.
+            res.redirect(category.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = function (req, res) {
--- controllers/tyreController.js
+++ controllers/tyreController.js
@@ -1,6 +1,9 @@
-// var async = require("async");
+var async = require("async");
+const { body, validationResult } = require("express-validator");
 
 var Tyre = require("../models/tyre");
+var Brand = require("../models/brand");
+var Category = require("../models/category");
 
 // Display list of all Tyres.
 exports.tyre_list = function (req, res) {
@@ -42,13 +45,114 @@ exports.tyre_detail = function (req, res, next) {
 
 // Display Tyre create form on GET.
 exports.tyre_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Tyre create GET");
+  // Get all brands and categories, which we can use for adding to our tyre.
+  async.parallel(
+    {
+      brands: function (callback) {
+        Brand.find(callback);
+      },
+      categories: function (callback) {
+        Category.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render("tyre_form", {
+        title: "Create Tyre",
+        brands: results.brands,
+        categories: results.categories,
+      });
+    }
+  );
 };
 
 // Handle Tyre create on POST.
-exports.tyre_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Tyre create POST");
-};
+exports.tyre_create_post = [
+  // Validate and sanitise fields.
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified."),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Description must be specified."),
+  body("stock_amount")
+    .trim()
+    .isInt({ min: 0 })
+    .withMessage("Stock amount must be a non-negative number."),
+  body("rating")
+    .trim()
+    .isInt({ min: 0, max: 5 })
+    .withMessage("Rating must be a number between 1 and 5."),
+  body("brand", "Brand must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel(
+        {
+          brands: function (callback) {
+            Brand.find(callback);
+          },
+          categories: function (callback) {
+            Category.find(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+
+          res.render("tyre_form", {
+            title: "Create Tyre",
+            brands: results.brands,
+            categories: results.categories,
+            tyre: tyre,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create a Tyre object with escaped and trimmed data.
+      var tyre = new Tyre({
+        name: req.body.name,
+        description: req.body.description,
+        stock_amount: req.body.stock_amount,
+        rating: req.body.rating,
+        brand: req.body.brand,
+        category: req.body.category,
+      });
+      tyre.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new tyre record.
+        res.redirect(tyre.url);
+      });
+    }
+  },
+];
 
 // Display Tyre delete form on GET.
 exports.tyre_delete_get = function (req, res) {
